240e3b4ede3d54e3d4001c8ba778ae5062d01fd5
Rishi Raj Singh <78776240+RishiRajSingh1@users.noreply.github.com>
2023-07-30 21:55:52 +0530

    commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,205 @@
+import React from 'react'
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main className="desktop-1" id="mainSection">
+    <div className="glowly">GLOWLY</div>
+    <div className="about">About</div>
+    <div className="products">Products</div>
+    <div className="blogs">Blogs</div>
+    <div className="contacts">Contacts</div>
+    <img
+      className="shopping-cart-1-icon"
+      alt=""
+      src="/shoppingcart-1@2x.png"
+    />
+    <div className="desktop-1-child" />
+    <div className="desktop-1-item" />
+    <div className="desktop-1-inner" />
+    <img className="loupe-1-icon" alt="" src="/loupe-1@2x.png" />
+    <div className="line-div" />
+    <img className="line-icon" alt="" src="/line-2.svg" />
+    <div className="glow-is-the">{`Glow is the Essence `}</div>
+    <div className="glowly-colourland-focus-on-container">
+      <p className="glowly-colourland">Glowly Colourland</p>
+      <p className="glowly-colourland">Focus On Me Blush</p>
+    </div>
+    <div className="meet-our-best">Meet Our Best Seller</div>
+    <div className="we-introduce-a">We Introduce A Glam Beauty Glow</div>
+    <div className="love-from-our">Love From Our Clients.</div>
+    <div className="of-beauty">of Beauty</div>
+    <img
+      className="purple-hair-girl-model-is-wear-icon"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-1@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon1"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-2@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon2"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-3@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon3"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-3@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon4"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-3@2x.png"
+    />
+    <div className="rectangle-div" />
+    <div className="desktop-1-child1" />
+    <div className="desktop-1-child2" />
+    <div className="desktop-1-child3" />
+    <div className="desktop-1-child4" />
+    <div className="desktop-1-child5" />
+    <img
+      className="inverted-commas-1-icon"
+      alt=""
+      src="/invertedcommas-1@2x.png"
+    />
+    <b className="a-girl-should">A girl should be two things,</b>
+    <b className="classy-and-fabulous">Classy and Fabulous</b>
+    <img className="star-1-icon" alt="" src="/star-1@2x.png" />
+    <img className="star-2-icon" alt="" src="/star-1@2x.png" />
+    <img className="star-4-icon" alt="" src="/star-1@2x.png" />
+    <img className="star-5-icon" alt="" src="/star-1@2x.png" />
+    <img className="star-3-icon" alt="" src="/star-1@2x.png" />
+    <div className="reviews">5000 Reviews</div>
+    <div className="desktop-1-child6" />
+    <div className="div">5.0</div>
+    <img className="download-1-1" alt="" src="/download-1-1@2x.png" />
+    <div className="ellipse-div" />
+    <div className="desktop-1-child7" />
+    <img className="polygon-icon" alt="" src="/polygon-1.svg" />
+    <div className="play-video">
+      <p className="glowly-colourland">{`Play `}</p>
+      <p className="glowly-colourland">Video</p>
+    </div>
+    <div className="scroll-down">
+      <p className="glowly-colourland">Scroll</p>
+      <p className="glowly-colourland"> Down</p>
+    </div>
+    <img className="arrow-icon" alt="" src="/arrow-1.svg" />
+    <img
+      className="diamond-symbol-1-icon"
+      alt=""
+      src="/diamondsymbol-1@2x.png"
+    />
+    <img
+      className="diamond-symbol-2-icon"
+      alt=""
+      src="/diamondsymbol-1@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon5"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-2@2x.png"
+    />
+    <div className="desktop-1-child8" />
+    <img
+      className="diamond-symbol-3-icon"
+      alt=""
+      src="/diamondsymbol-1@2x.png"
+    />
+    <img
+      className="diamond-symbol-4-icon"
+      alt=""
+      src="/diamondsymbol-1@2x.png"
+    />
+    <img
+      className="purple-hair-girl-model-is-wear-icon6"
+      alt=""
+      src="/purple-hair-girl-model-is-wearing-dark-brown-dress-standing-in-light-pink-background-hd-girls1280x720-7@2x.png"
+    />
+    <div className="desktop-1-child9" />
+    <img
+      className="diamond-symbol-5-icon"
+      alt=""
+      src="/diamondsymbol-5@2x.png"
+    />
+    <img
+      className="diamond-symbol-6-icon"
+      alt=""
+      src="/diamondsymbol-5@2x.png"
+    />
+    <div className="lorem-ipsum-dolor">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime deleniti corporis tempore blanditiis alias mollitia,
+      repudiandae facere.
+    </div>
+    <div className="lorem-ipsum-dolor1">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime.
+    </div>
+    <div className="lorem-ipsum-dolor2">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime.
+    </div>
+    <div className="lorem-ipsum-dolor3">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime.
+    </div>
+    <div className="lorem-ipsum-dolor4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime deleniti corporis tempore blanditiis alias mollitia,
+      repudiandae facere.
+    </div>
+    <div className="lorem-ipsum-dolor5">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime deleniti corporis tempore blanditiis alias mollitia,
+      repudiandae facere.
+    </div>
+    <div className="lorem-ipsum-dolor6">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+      impedit amet maxime deleniti corporis tempore blanditiis alias mollitia,
+      repudiandae facere.
+    </div>
+    <div className="desktop-1-child10" />
+    <div className="desktop-1-child11" />
+    <div className="benefit">Benefit</div>
+    <div className="price">Price</div>
+    <b className="skin-fine">Skin-Fine</b>
+    <b className="b">$25.99</b>
+    <b className="b1">$25.99</b>
+    <b className="b2">$25.99</b>
+    <b className="b3">$25.99</b>
+    <div className="desktop-1-child12" />
+    <b className="shop-now">Shop Now</b>
+    <div className="desktop-1-child13" />
+    <div className="desktop-1-child13" />
+    <b className="shop-now1">Shop Now</b>
+    <div className="desktop-1-child15" />
+    <div className="desktop-1-child15" />
+    <div className="desktop-1-child15" />
+    <b className="cruelty-free">Cruelty Free</b>
+    <div className="desktop-1-child18" />
+    <div className="desktop-1-child18" />
+    <b className="travel-friendly">Travel Friendly</b>
+    <div className="desktop-1-child20" />
+    <div className="desktop-1-child20" />
+    <b className="durable">Durable</b>
+    <div className="desktop-1-child22" />
+    <div className="desktop-1-child22" />
+    <b className="shop-now2">Shop Now</b>
+    <div className="desktop-1-child24" />
+    <div className="desktop-1-child24" />
+    <b className="shop-now3">Shop Now</b>
+    <div className="desktop-1-child26" />
+    <b className="explore">Explore</b>
+    <div className="desktop-1-child27" />
+    <img className="desktop-1-child28" alt="" src="/arrow-2.svg" />
+    <div className="desktop-1-child29" />
+    <img className="desktop-1-child30" alt="" src="/arrow-3.svg" />
+  </main>
+  )
+}
+
+export default Main
